6c06bfc2feecf9666c44b82617cbcb2a65ed2189
ScriptAndCode <10951705@uvu.edu>
2023-02-09 17:03:59 -0700

    Created PlayerController.cs

---
+++ UFO-DefenseForceGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 50.00f;
+    public float xRange = 25.00f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set horizontalInput to recieve values from keyboard:
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Moves Player left and right:
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        //  Keep player within bounds, left side:
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        //  Keep player within bounds, right side:
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
