d3e9b89e6568e90c2048986f0dfb18274d9d7d69
ScriptAndCode <10951705@uvu.edu>
2023-02-11 09:32:46 -0700

    Blaster shoots, and destroys out of bounds laser bolts

--- UFO-DefenseForceGame/Assets/Scripts/PlayerController.cs
+++ UFO-DefenseForceGame/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 50.00f;
     public float xRange = 25.00f;
+    public Transform blaster;
+    public GameObject laserBolt;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +35,13 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        // If space bar is pressed, fire laserBolt:
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create lasberBolt at the blaster location:
+            Instantiate(laserBolt, blaster.transform.position, laserBolt.transform.rotation);
         }
     }
 }
