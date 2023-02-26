bf5b10ad84995b72e72010021662b7d2fab0f888
ScriptAndCode <10951705@uvu.edu>
2023-02-26 11:43:36 -0700

    New script to change color of the PickupItem

---
+++ UFO-DefenseForceGame/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+
+    Color colorStart = Color.yellow;
+    Color colorEnd = Color.red;
+    float duration = 0.5f;
+    Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        rend.material.color = Color.Lerp(colorStart, colorEnd, lerp);
+    }
+}
